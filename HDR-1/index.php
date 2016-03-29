23cad17878a3544e48660446140b7bb68deffcbb
jose nazario <jose.monkey.org@gmail.com>
2016-03-28 21:20:09 -0400

    call the right thing, fixes issue #2

--- HDR-1/index.php
+++ HDR-1/index.php
@@ -4,7 +4,7 @@
 <?php hint("will include the arg specified in the HTTP Cookie parameter \"TEMPLATE\""); ?>
 <?php
 $template = 'blue.php';
-if ( is_set( $_COOKIE['TEMPLATE'] ) )
+if ( array_key_exists( $_COOKIE['TEMPLATE'] ) )
    $template = $_COOKIE['TEMPLATE'];
 include ( "/Users/josenazario/templates/" . $template );
 ?>
